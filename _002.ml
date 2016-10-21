0bdbb6703acd72f05bd779b4e272012c09900b06
kammitama5 <krystal.maughan@gmail.com>
2016-10-21 09:53:43 -0700

    1st commit
    array head and reverse

---
+++ _002.ml
@@ -0,0 +1,8 @@
+(* List reverse*)
+List.rev[2; 4; 6];; (*outputs int list = [6; 4; 1]*)
+
+(* First element or head of list *)
+List.hd[2; 4; 6];; (*outputs int = 2]*)
+
+(* Append *)
+Array.append[|2; 4|][|3; 4; 5|];; (*outputs [|2; 4; 3; 4; 5|])
