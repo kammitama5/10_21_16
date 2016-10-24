8361e122a0cb7070e8f2404324b1647085d95098
kammitama5 <krystal.maughan@gmail.com>
2016-10-24 07:51:12 -0700

    1st commit

---
+++ _008.ml
@@ -0,0 +1,10 @@
+(* corrected Ocaml *)
+let fernand = "King of Castille";;
+
+let rodrigue = "the cid";;
+
+let diegue = "cid's father";;
+
+let characters = [fernand; rodrigue; diegue];;
+
+characters;;
